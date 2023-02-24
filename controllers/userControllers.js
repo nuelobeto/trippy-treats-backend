fea79dca6ef94ba86cbc5de487650a3beca5d016
nuelobeto <nuelobeto@gmail.com>
2023-02-24 08:36:40 +0100

    added update password api

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -75,7 +75,7 @@ const login = asyncHandler(async (req, res) => {
     throw new Error("Invalid Credentials");
   }
 
-  res.status(201).json({
+  res.status(200).json({
     id: user.id,
     name: user.name,
     email: user.email,
@@ -105,7 +105,7 @@ const verifyEmail = asyncHandler(async (req, res) => {
     throw new Error("Invalid link");
   }
 
-  const updatedUser = await User.findOneAndUpdate(
+  await User.findOneAndUpdate(
     {
       _id: req.params.id,
     },
@@ -119,6 +119,84 @@ const verifyEmail = asyncHandler(async (req, res) => {
 
   const redirectUrl = req.query.redirect;
   res.redirect(redirectUrl);
+});
+
+const sendResetPasswordLink = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    res.status(400);
+    throw new Error("Please enter your email");
+  }
+
+  const user = await User.findOne({
+    email: new RegExp("^" + email + "$", "i"),
+  });
+
+  if (!user) {
+    res.status(400);
+    throw new Error("This user does not exist");
+  }
+
+  const token = await Token.create({
+    user: user.id,
+    token: crypto.randomBytes(32).toString("hex"),
+  });
+
+  const message = `
+  <h3>Password Reset</h3>
+  <p>Please click the link below to reset your password.</p>
+  <a href="${process.env.BASE_URL}/reset-password/${user.id}/${token.token}?redirect=${process.env.FRONTEND_URL}/reset-password/${user.id}/${token.token}">Reset password</a>
+  `;
+
+  await sendEmail(user.email, "Password reset", message);
+
+  res.status(200).json({
+    message: "An email has been sent to your account",
+  });
+});
+
+const verifyResetPasswordLink = asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+    _id: req.params.id,
+  });
+
+  if (!user) {
+    res.status(400);
+    throw new Error("Invalid link");
+  }
+
+  const token = await Token.findOne({
+    user: user.id,
+    token: req.params.token,
+  });
+
+  if (!token) {
+    res.status(400);
+    throw new Error("Invalid link");
+  }
+
+  const redirectUrl = req.query.redirect;
+  res.redirect(redirectUrl);
+});
+
+const updatePassword = asyncHandler(async (req, res) => {
+  const { password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const updatedUser = await User.findOneAndUpdate(
+    {
+      _id: req.params.id,
+    },
+    {
+      password: hashedPassword,
+    },
+    {
+      new: true,
+    }
+  );
 
   res.status(200).json({
     id: updatedUser.id,
@@ -138,4 +216,7 @@ module.exports = {
   register,
   login,
   verifyEmail,
+  sendResetPasswordLink,
+  verifyResetPasswordLink,
+  updatePassword,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,16 @@ const {
   register,
   login,
   verifyEmail,
+  sendResetPasswordLink,
+  verifyResetPasswordLink,
+  updatePassword,
 } = require("../controllers/userControllers");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/verify/:id/:token", verifyEmail);
+router.post("/forgot-password", sendResetPasswordLink);
+router.get("/reset-password/:id/:token", verifyResetPasswordLink);
+router.put("/update-password/:id", updatePassword);
 
 module.exports = router;
